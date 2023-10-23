ea114733da9c4b9120bb34a07053de0c50e582c0
Amanda <amanda.ausejo@gmail.com>
2023-10-23 14:57:34 +0200

    feat Il Calcolatore di Età

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 // 1. *Il Caffè*: Chiedi all'utente quante tazze di caffè ha venduto oggi. Calcola il totale dei guadagni per il giorno e visualizza un messaggio con il risultato.
-let caffèVenduto = prompt("Quante tazze di caffè hai venduto oggi?");
-let totVenduti = caffèVenduto;
-let costoCaffè = 2;
-let guadagni = caffèVenduto * costoCaffè;
-alert(guadagni);
+// let caffèVenduto = prompt("Quante tazze di caffè hai venduto oggi?");
+// let totVenduti = caffèVenduto;
+// let costoCaffè = 2;
+// let guadagni = caffèVenduto * costoCaffè;
+// alert(guadagni);
+
+// 2. *Il Calcolatore di Età*: Chiedi all'utente il loro anno di nascita e calcola la loro età. Visualizza un messaggio con il risultato.
+let annoNascita = prompt("In che anno sei nato?");
+let anno = annoNascita;
+let età = 2023 - anno;
+alert(età);
