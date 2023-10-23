9867fe9c85c293cd1f1ea7f354742c60cd5ed089
Amanda <amanda.ausejo@gmail.com>
2023-10-23 15:45:21 +0200

    feat Il Calcolatore di Mancia

--- main.js
+++ main.js
@@ -22,19 +22,27 @@
 
 // 624. *Il Calcolatore di Voti*: Chiedi i voti del test di 3 studenti, calcola la media e visualizza il risultato.
 
-let domandaStudente1 = prompt(
-    "Che voto ai preso nel test di biologia? (studente 1)"
-);
-let domandaStudente2 = prompt(
-    "Che voto ai preso nel test di biologia? (studente 2)"
-);
+// let domandaStudente1 = prompt(
+//     "Che voto ai preso nel test di biologia? (studente 1)"
+// );
+// let domandaStudente2 = prompt(
+//     "Che voto ai preso nel test di biologia? (studente 2)"
+// );
 
-let domandaStudente3 = prompt(
-    "Che voto ai preso nel test di biologia? (studente 3)"
-);
-22;
-let votoStud1 = parseInt(domandaStudente1);
-let votoStud2 = parseInt(domandaStudente2);
-let votoStud3 = parseInt(domandaStudente3);
-let mediaVoti = (votoStud1 + votoStud2 + votoStud3) / 3;
-alert("La media dei voti è: " + mediaVoti);
+// let domandaStudente3 = prompt(
+//     "Che voto ai preso nel test di biologia? (studente 3)"
+// );
+// 22;
+// let votoStud1 = parseInt(domandaStudente1);
+// let votoStud2 = parseInt(domandaStudente2);
+// let votoStud3 = parseInt(domandaStudente3);
+// let mediaVoti = (votoStud1 + votoStud2 + votoStud3) / 3;
+// alert("La media dei voti è: " + mediaVoti);
+
+// 5. *Il Calcolatore di Mancia*: Chiedi all'utente il totale del conto e la percentuale di mancia. Calcola la mancia e visualizza il risultato.
+let domTotConto = prompt("Qual'è il totale del conto?");
+let domPercentualeMancia = prompt("Qual'è la percentuale della mancia?");
+let totaleConto = domTotConto;
+let percentualeMancia = (parseFloat(domPercentualeMancia6060) / 100) * 100;
+let mancia = (percentualeMancia / 100) * totaleConto;
+alert(mancia);
