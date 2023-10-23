36bb33bb3483e4b4ffb59a993a8991c37f556a3d
Amanda <amanda.ausejo@gmail.com>
2023-10-23 14:47:03 +0200

    feat Il Caffè

---
+++ main.js
@@ -0,0 +1,6 @@
+// 1. *Il Caffè*: Chiedi all'utente quante tazze di caffè ha venduto oggi. Calcola il totale dei guadagni per il giorno e visualizza un messaggio con il risultato.
+let caffèVenduto = prompt("Quante tazze di caffè hai venduto oggi?");
+let totVenduti = caffèVenduto;
+let costoCaffè = 2;
+let guadagni = caffèVenduto * costoCaffè;
+alert(guadagni);
